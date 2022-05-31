24fd401963f9a2ead512749f2e61500c82a2c0e2
Aaron Ky-Riesenbach <aaronkyriesenbach@pobox.com>
2022-06-01 00:44:45 +0200

    Attempt to fix Instagram load issues

--- src/components/useGetMobile.ts
+++ src/components/useGetMobile.ts
@@ -2,12 +2,15 @@ import { useEffect, useState } from 'react';
 
 export default function useGetMobile() {
     const [width, setWidth] = useState(0);
+    const [screenWidth, setScreenWidth] = useState(0);
 
-    const getWidth = () => {
+    const getValues = () => {
         setWidth(window.innerWidth);
+        setScreenWidth(window.screen.width);
     }
 
-    useEffect(() => getWidth(), []);
+    useEffect(() => getValues(), []);
 
-    return width <= 768;
+    // Properly load when Instagram app arbitrarily sets window.innerWidth
+    return (width > screenWidth ? screenWidth : width) <= 768;
 }
